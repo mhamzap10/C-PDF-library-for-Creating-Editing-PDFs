494869975928a9f4939b68d97c039de004d9b168
hamza <mhamzap10@gmail.com>
2023-10-31 11:47:52 +0500

    Create PDF in C# - MVC Project

---
+++ CreatePDF/CreatePDF/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using CreatePDF.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CreatePDF.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public FileResult CreatePDF(CreatePDFModel model)
+        {
+            var renderer = new ChromePdfRenderer();
+            PdfDocument pdfDoc;
+            if (model.isURL)
+            {
+                pdfDoc = renderer.RenderUrlAsPdf(model.Content);
+            }
+            else
+            {
+                pdfDoc = renderer.RenderHtmlAsPdf(model.Content);
+            }
+            byte[] fileBytes = pdfDoc.BinaryData;
+            string fileName = "myPDF.pdf";
+            return File(fileBytes, System.Net.Mime.MediaTypeNames.Application.Pdf, fileName);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ CreatePDF/CreatePDF/Models/CreatePDFModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace CreatePDF.Models
+{
+    public class CreatePDFModel
+    {
+        [DataType(DataType.MultilineText)]
+        public string Content { get; set; }
+        [DisplayName("Create From URL")]
+        public bool isURL { get; set; }
+    }
+}
